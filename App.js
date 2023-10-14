2ff84f55b18f030610969578ccade6e7854a4909
Sid <siddharthpillai123@gmail.com>
2023-10-14 17:21:12 -0400

    feat: auth

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
-import HomeScreen from './src/screens/HomeScreen/HomeScreen'
+import LoginScreen from './src/screens/LoginScreen/LoginScreen'
+import InsideLayout from './src/screens/index'
 
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import { useState, useEffect } from 'react'
+
+import { auth } from './src/firebase/config'
+
+const Stack = createStackNavigator()
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(()=>{
+    auth.onAuthStateChanged((user)=>{
+      console.log("User: " + user)
+      setUser(user)
+    })
+  },[])
+
+  // login screen if user exists, else move in index.js 
   return (
-    <HomeScreen></HomeScreen>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        {user? (
+          <Stack.Screen name= "InsideStack" component={InsideLayout} options={{headerShown:false}}/>
+        ):(
+          <Stack.Screen name= "LoginScreen" component={LoginScreen} options={{headerShown:false}}/>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 export default App;
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,12 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
+import "firebase/compat/auth";
 import firebaseConfig from "../../keys/firebase_key.json"; // Adjust the path to where your config.json file is
 
 
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-export { app, db }
+const auth = firebase.auth()
+
+export { app, auth, db }
