9ab03b1339cc8cb1a78ed39792e86d23a9f05cc5
Shreyas Raikhelkar <shreyas.raikhelkar@gmail.com>
2023-10-15 05:37:26 -0400

    Improved splash screen and login card

--- src/screens/LoginScreen/components/LoginCard.js
+++ src/screens/LoginScreen/components/LoginCard.js
@@ -5,55 +5,54 @@ import {
   KeyboardAvoidingView,
   View,
   TouchableOpacity,
-  Text
+  Text,
 } from "react-native";
 import { StyleSheet, Dimensions } from "react-native";
 import { useState } from "react";
 import { auth } from "../../../firebase/config";
 
 export default LoginCard = (props) => {
+  const { color } = props;
+  const windowWidth = Dimensions.get("window").width;
+  const windowHeight = Dimensions.get("window").height;
 
-const { color } = props;
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    top: 0.05 * windowHeight,
-    alignItems: "center",
-    backgroundColor: "black",
-  },
-  input: {
-    width: 300,
-    height: 40,
-    borderColor: 'grey',
-    borderWidth: 1,
-    margin: 10,
-    paddingLeft: 10,
-    color: 'white',
-  },
-  buttonContainer: {
-    width: 0.4 * windowWidth,
-    height: 0.07 * windowHeight,
-    marginBottom: 10,
-    marginTop: 10,
-  },
-  button: {
-    backgroundColor: 'black',
-    borderColor: color,
-    borderWidth: 1,
-    borderRadius: 12,
-    padding: 5,
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1,
-  },
-  buttonText: {
-    color: color,
-    fontSize: 18,
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      top: 0.05 * windowHeight,
+      alignItems: "center",
+      backgroundColor: "black",
+    },
+    input: {
+      width: 300,
+      height: 40,
+      borderColor: "grey",
+      borderWidth: 1,
+      margin: 10,
+      paddingLeft: 10,
+      color: "white",
+    },
+    buttonContainer: {
+      width: 0.4 * windowWidth,
+      height: 0.07 * windowHeight,
+      marginBottom: 10,
+      marginTop: 10,
+    },
+    button: {
+      backgroundColor: "black",
+      borderColor: color,
+      borderWidth: 1,
+      borderRadius: 12,
+      padding: 5,
+      justifyContent: "center",
+      alignItems: "center",
+      flex: 1,
+    },
+    buttonText: {
+      color: color,
+      fontSize: 18,
+    },
+  });
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -91,6 +90,7 @@ const styles = StyleSheet.create({
           style={styles.input}
           value={email}
           placeholder="email"
+          placeholderTextColor="white"
           autoCapitalize="none"
           onChangeText={(text) => setEmail(text)}
         ></TextInput>
@@ -99,6 +99,7 @@ const styles = StyleSheet.create({
           value={password}
           secureTextEntry={true}
           placeholder="password"
+          placeholderTextColor="white"
           autoCapitalize="none"
           onChangeText={(text) => setPassword(text)}
         ></TextInput>
@@ -108,21 +109,15 @@ const styles = StyleSheet.create({
         <ActivityIndicator></ActivityIndicator>
       ) : (
         <>
-        <TouchableOpacity
-            style={styles.buttonContainer}
-            onPress={signIn}
-          >
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>Login</Text>
-          </View>
+          <TouchableOpacity style={styles.buttonContainer} onPress={signIn}>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>Login</Text>
+            </View>
           </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.buttonContainer}
-            onPress={signIn}
-          >
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>Create Account</Text>
-          </View>
+          <TouchableOpacity style={styles.buttonContainer} onPress={signIn}>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>Create Account</Text>
+            </View>
           </TouchableOpacity>
         </>
       )}
--- src/screens/StartScreen/StartScreen.js
+++ src/screens/StartScreen/StartScreen.js
@@ -1,89 +1,92 @@
-import { Animated, StyleSheet, Text, View, Dimensions, Button } from 'react-native';
-import React, { useRef, useEffect, useState } from 'react';
-import { useFonts } from 'expo-font';
-
-
+import {
+  Animated,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Button,
+} from "react-native";
+import React, { useRef, useEffect, useState } from "react";
+import { useFonts } from "expo-font";
 
 export default function StartScreen({ navigation }) {
-
   const [fontsLoaded] = useFonts({
-    'font': require('../../../assets/fonts/mexcellent-rg.otf'),
+    font: require("../../../assets/fonts/mexcellent-rg.otf"),
   });
 
-  const colors = ['red', 'orange', 'yellow', 'green', 'cyan', 'violet']
-  generateRandomNumber = () => { 
-    const min = 0; 
-    const max = colors.length - 1; 
+  const colors = ["red", "orange", "yellow", "green", "cyan", "violet"];
+  generateRandomNumber = () => {
+    const min = 0;
+    const max = colors.length - 1;
     return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+  };
 
   const randomNumber = useRef(generateRandomNumber()).current;
   let neonLight = useRef(new Animated.Value(0)).current;
-  
+
   const styles = StyleSheet.create({
     container: {
-      backgroundColor: 'black',
-      alignItems: 'center',
-      justifyContent: 'center',
-      position: 'absolute',
-      height: Dimensions.get('window').height,
-      width: Dimensions.get('window').width,
+      backgroundColor: "black",
+      alignItems: "center",
+      justifyContent: "center",
+      position: "absolute",
+      height: "100%",
+      width: Dimensions.get("window").width,
     },
     neon: {
-        fontSize: 90,
-        fontFamily: 'font',
-        color: colors[randomNumber],
-        textShadowColor: colors[randomNumber],
-        textShadowOffset: { width: 0, height: 0 },
-        textShadowRadius: 30,
-        opacity: neonLight
+      fontSize: 90,
+      fontFamily: "font",
+      color: colors[randomNumber],
+      textShadowColor: colors[randomNumber],
+      textShadowOffset: { width: 0, height: 0 },
+      textShadowRadius: 30,
+      opacity: neonLight,
     },
   });
 
   useEffect(() => {
-
-      Animated.sequence([
-        Animated.timing(neonLight, {
-          toValue: 1,
-          duration: 500,
-          useNativeDriver: false,
-        }),
-        Animated.timing(neonLight, {
-          toValue: 0,
-          duration: 100,
-          useNativeDriver: false,
-        }),
-        Animated.timing(neonLight, {
-          toValue: 1,
-          duration: 500,
-          useNativeDriver: false,
-        }),
-        Animated.timing(neonLight, {
-          toValue: 0,
-          duration: 100,
-          useNativeDriver: false,
-        }),
-        Animated.timing(neonLight, {
-          toValue: 1,
-          duration: 500,
-          useNativeDriver: false,
-        }),
-        Animated.timing(neonLight, {
-          toValue: 0,
-          duration: 100,
-          useNativeDriver: false,
-        }),
-        Animated.timing(neonLight, {
-          toValue: 1,
-          duration: 500,
-          useNativeDriver: false,
-        }),
-      ]).start()
+    Animated.sequence([
+      Animated.timing(neonLight, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: false,
+      }),
+      Animated.timing(neonLight, {
+        toValue: 0,
+        duration: 100,
+        useNativeDriver: false,
+      }),
+      Animated.timing(neonLight, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: false,
+      }),
+      Animated.timing(neonLight, {
+        toValue: 0,
+        duration: 100,
+        useNativeDriver: false,
+      }),
+      Animated.timing(neonLight, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: false,
+      }),
+      Animated.timing(neonLight, {
+        toValue: 0,
+        duration: 100,
+        useNativeDriver: false,
+      }),
+      Animated.timing(neonLight, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: false,
+      }),
+    ]).start();
 
     const timer = setTimeout(() => {
       navigation.navigate("Login Screen", {
         color: colors[randomNumber],
-        font: 'font',
+        font: "font",
       });
     }, 5000);
 
