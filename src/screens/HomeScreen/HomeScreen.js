27b3e951f0d9da9fe9b086746025d826c04ad7bd
Ravichandran, Rishi Ganesh <rgr7@gatech.edu>
2023-10-15 03:11:36 -0400

    Dark mode main screen and bottom nav

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -4,69 +4,117 @@ import FirebaseAPI from "../../firebase/firebaseAPI";
 import styles from "./styles";
 import ListItem from "./components/ListItem";
 import FloatingActionButton from "./components/FloatingActionButton";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { StyleSheet, Dimensions } from "react-native";
+// import Icon from "react-native-vector-icons/FontAwesome";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const Tab = createBottomTabNavigator();
 
+const eventStyles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+    display: "flex",
+    flex: 1,
+    backgroundColor: "black",
+    // flexDirection: 'column'
+  },
+  list: {
+    position: "absolute",
+    width: 1.0 * windowWidth,
+    // alignItems: 'center'
+  },
+});
+
 const MapScreen = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
     <Text>Map</Text>
   </View>
 );
 
-const EventScreen = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Events</Text>
-  </View>
-);
+const EventScreen = ({ navigation }) => {
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      const fetchEvents = async () => {
+        const eventData = await FirebaseAPI.readEvents();
+        setEvents(eventData);
+        setLoading(false);
+      };
+      fetchEvents();
+    });
+    return unsubscribe;
+  }, [navigation]);
+  if (!loading) {
+    return (
+      <SafeAreaView style={eventStyles.container}>
+        <FlatList
+          style={eventStyles.list}
+          data={events}
+          renderItem={(event) => <ListItem item={event.item} />}
+          keyExtractor={(event) => event.id}
+        />
+        <FloatingActionButton
+          onPressItem={() => navigation.navigate("Make a Move")}
+        />
+      </SafeAreaView>
+    );
+  } else {
+    return <View></View>;
+  }
+};
 
 const HomeScreen = ({ navigation }) => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Map" component={MapScreen} options={{ headerShown: false, tabBarIcon:
-            <Icon name="map-pin" size={30} color='white' /> }}/>
-      <Tab.Screen name="Event" component={EventScreen} options={{ headerShown: false, tabBarIcon:
-            <Icon name="event" size={30} color='white' /> }}/>
+      <Tab.Screen
+        name="Map"
+        component={MapScreen}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Event"
+        component={EventScreen}
+        options={{ headerShown: false }}
+      />
     </Tab.Navigator>
   );
 };
 
+// const [events, setEvents] = useState([]);
+// const [loading, setLoading] = useState(true);
 
+// useEffect(() => {
+//   const unsubscribe = navigation.addListener("focus", () => {
+//     const fetchEvents = async () => {
+//       const eventData = await FirebaseAPI.readEvents();
+//       setEvents(eventData);
+//       setLoading(false);
+//     };
+//     fetchEvents();
+//   });
 
-  
+//   return unsubscribe;
+// }, [navigation]);
 
-  // const [events, setEvents] = useState([]);
-  // const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   const unsubscribe = navigation.addListener("focus", () => {
-  //     const fetchEvents = async () => {
-  //       const eventData = await FirebaseAPI.readEvents();
-  //       setEvents(eventData);
-  //       setLoading(false);
-  //     };
-  //     fetchEvents();
-  //   });
-
-  //   return unsubscribe;
-  // }, [navigation]);
-
-  // if (!loading) {
-  //   return (
-  //     <SafeAreaView style={styles.container}>
-  //       <FlatList
-  //         style={styles.list}
-  //         data={events}
-  //         renderItem={(event) => <ListItem item={event.item} />}
-  //         keyExtractor={(event) => event.id}
-  //       />
-  //       <FloatingActionButton
-  //         onPressItem={() => navigation.navigate("Make a Move")}
-  //       />
-  //     </SafeAreaView>
-  //   );
-  // } else {
-  //   return <View></View>;
-  // }
+// if (!loading) {
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <FlatList
+//         style={styles.list}
+//         data={events}
+//         renderItem={(event) => <ListItem item={event.item} />}
+//         keyExtractor={(event) => event.id}
+//       />
+//       <FloatingActionButton
+//         onPressItem={() => navigation.navigate("Make a Move")}
+//       />
+//     </SafeAreaView>
+//   );
+// } else {
+//   return <View></View>;
+// }
 export default HomeScreen;
