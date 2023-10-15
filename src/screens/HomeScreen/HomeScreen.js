4c01bdbc77d7eddf493ac8dc5b6c5c23328d22ad
vmirchandani6 <vmirchandani6@gatech.edu>
2023-10-15 01:26:54 -0400

    added branch

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -6,38 +6,41 @@ import ListItem from "./components/ListItem";
 import FloatingActionButton from "./components/FloatingActionButton";
 
 const HomeScreen = ({ navigation }) => {
-  const [events, setEvents] = useState([]);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const unsubscribe = navigation.addListener("focus", () => {
-      const fetchEvents = async () => {
-        const eventData = await FirebaseAPI.readEvents();
-        setEvents(eventData);
-        setLoading(false);
-      };
-      fetchEvents();
-    });
+  
 
-    return unsubscribe;
-  }, [navigation]);
+  // const [events, setEvents] = useState([]);
+  // const [loading, setLoading] = useState(true);
 
-  if (!loading) {
-    return (
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          style={styles.list}
-          data={events}
-          renderItem={(event) => <ListItem item={event.item} />}
-          keyExtractor={(event) => event.id}
-        />
-        <FloatingActionButton
-          onPressItem={() => navigation.navigate("Make a Move")}
-        />
-      </SafeAreaView>
-    );
-  } else {
-    return <View></View>;
-  }
+  // useEffect(() => {
+  //   const unsubscribe = navigation.addListener("focus", () => {
+  //     const fetchEvents = async () => {
+  //       const eventData = await FirebaseAPI.readEvents();
+  //       setEvents(eventData);
+  //       setLoading(false);
+  //     };
+  //     fetchEvents();
+  //   });
+
+  //   return unsubscribe;
+  // }, [navigation]);
+
+  // if (!loading) {
+  //   return (
+  //     <SafeAreaView style={styles.container}>
+  //       <FlatList
+  //         style={styles.list}
+  //         data={events}
+  //         renderItem={(event) => <ListItem item={event.item} />}
+  //         keyExtractor={(event) => event.id}
+  //       />
+  //       <FloatingActionButton
+  //         onPressItem={() => navigation.navigate("Make a Move")}
+  //       />
+  //     </SafeAreaView>
+  //   );
+  // } else {
+  //   return <View></View>;
+  // }
 };
 export default HomeScreen;
